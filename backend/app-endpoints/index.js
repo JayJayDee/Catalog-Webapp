f8723728798bd8095768436dca140b32945ba6d6
JayJayDee <jindongp@gmail.com>
2019-12-30 15:24:35 +0900

    added express-aplication, added cors middleware.

--- backend/app-endpoints/index.js
+++ backend/app-endpoints/index.js
@@ -1,6 +1,24 @@
-
+const { Router } = require('express'); 
 const { catalog } = require('./catalog');
+const { log } = require('../logger');
 
-module.exports = [
-  ...catalog()
-];
+const tag = '[app-endpoints]';
+
+const initAppEndpoints =
+  () => {
+    const router = new Router();
+    const endpoints = [
+      ...catalog()
+    ];
+
+    endpoints.forEach((e) => {
+      log.info(`${tag} ${e.method} ${e.uri} registered.`);
+      router[e.method].apply(router, [ e.uri, e.handlers ]);
+    });
+
+    return router;
+  };
+
+module.exports = {
+  initAppEndpoints
+};
